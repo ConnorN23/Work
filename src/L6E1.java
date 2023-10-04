e91c1fcc4eb9eca40455c2721de294f37592ffe4
Connor <30211023@nescol.ac.uk>
2023-10-04 11:00:56 +0100

    Week 5 Updated Code

---
+++ src/L6E1.java
@@ -0,0 +1,18 @@
+/**
+ * Java Program - Display Even Numbers
+ */
+
+public class L6E1 {
+
+    public static void main(String[] args) {
+        //number
+        int n = 24;
+
+        //print all even numbers <=n
+        int even=2;
+        while (even<=n) {
+            System.out.println(even);
+            even += 2;
+        }
+    }
+}
---
+++ src/L6E2.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+import java.util.Scanner;
+import java.time.LocalTime; // import the LocalTime class
+//.
+public class L6E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Say Hello");
+            System.out.println("2. Tell Me The Time");
+            System.out.println("3. Tell Me A Joke");
+            System.out.println("4. Quit");
+            String answer1 = input.nextLine();
+            if (answer1.equalsIgnoreCase("4")) {
+                    System.exit(0); }
+            }
+        }
+    }
